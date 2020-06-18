227827f6c71bca2a07c0db7706eb08d3fe1ff15b
Bahzad-Ahmed <54814375+Bahzad-Ahmed@users.noreply.github.com>
2020-06-18 21:11:45 +0500

    Create Task_8.m
    early stages

---
+++ Task_8.m
@@ -0,0 +1,61 @@
+% Random walk inside circle using continuous random variables
+clc;
+close all;
+clear;
+
+stepsPerWalk = [];
+numberOfWalks = 100;
+
+% Plotting a circle
+startingXpos = 0;
+startingYpos = 0;
+radius = 100;
+diameter = radius*2;
+px = startingXpos - radius;
+py = startingYpos - radius;
+rectangle('Position',[px, py, diameter, diameter],'Curvature',[1 1])
+axis square;
+hold on;
+
+for m = 1 : numberOfWalks
+    angle1=degtorad(360*rand(1));
+    angle2=degtorad(360*rand(1));
+    pos1=100*rand(1);
+    pos2=100*rand(1);
+    x_t_1(1)=pos1*sin(angle1);
+    y_t_1(1)=pos1*cos(angle1);
+    x_t_2(1)=pos2*sin(angle2);
+    y_t_2(1)=pos2*cos(angle2);
+    n= 1;
+    while sqrt((x_t_1(n)-x_t_2(n))^2+(y_t_1(n)-y_t_2(n))^2)>=1
+        ang1 = degtorad(360*rand(1));
+        stepSize1 = 1*rand(1);
+        dist_rad_1=sqrt((x_t_1(n) + stepSize1*sin(ang1))^2+(y_t_1(n) + stepSize1*cos(ang1))^2);
+        if dist_rad_1<100
+            x_t_1(n+1) = x_t_1(n) + stepSize1*sin(ang1);
+            y_t_1(n+1) = y_t_1(n) + stepSize1*cos(ang1);
+        else
+            x_t_1(n+1)=x_t_1(n);
+            y_t_1(n+1)=y_t_1(n);
+        end
+         
+        ang2 = degtorad(360*rand(1));
+        stepSize2 = 1*rand(1);
+        dist_rad_2=sqrt((x_t_2(n) + stepSize2*sin(ang2))^2+(y_t_2(n) + stepSize2*cos(ang2))^2);
+        if dist_rad_2<100
+            x_t_2(n+1) = x_t_2(n) + stepSize2*sin(ang2);
+            y_t_2(n+1) = y_t_2(n) + stepSize2*cos(ang2);
+        else
+            x_t_2(n+1)=x_t_2(n);
+            y_t_2(n+1)=y_t_2(n);
+        end
+        n=n+1;
+    end
+    stepsPerWalk(m)=n;
+    plot(x_t_1, y_t_1, 'LineWidth', 2);
+    plot(x_t_2,y_t_2,'LineWidth',2);
+  	hold on
+  	drawnow;
+end
+grid on
+meansteps=mean(stepsPerWalk)
